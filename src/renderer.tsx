3fec3f3525ef4655150ef3c563a08bb61518611a
stordahl <jacob@stordahl.dev>
2024-04-02 13:54:54 +0000

    feat: add counterscale scripts

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -30,6 +30,13 @@ export const renderer = jsxRenderer(
 							<span>© {new Date().getFullYear()} Jacob Stordahl</span>
 						</div>
 					</footer>
+          {import.meta.env.PROD &&
+            html`<script src="/static/js/counterscale-client.js" defer/>
+            <script
+              id="counterscale-script"
+              src="https://counterscale.stordahldev.workers.dev/tracker.js"
+              defer
+          />`}
 					{loadMermaid && html`
             <script type="module" async defer>
               import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';

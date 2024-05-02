852390ea32221c94843872528dc0c8325c69a2eb
stordahl <jacob@stordahl.dev>
2024-05-02 14:23:09 +0000

    feat: add feedback for email link copying

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -5,21 +5,28 @@
 function decorateMailToLinks() {
   const links = [...document.querySelectorAll('[href^="mailto:"]')]
   for (const link of links) {
-    link.addEventListener("click", (event) => {
+    link.addEventListener("click", async (event) => {
       event.preventDefault()
-      writeClipboardText("jacob@stordahl.dev") 
+      const success = await writeClipboardText("jacob@stordahl.dev") 
+      if (success) {
+        link.classList.add("copied")
+        setTimeout(() => (link.classList.remove("copied")), 3000)
+      } else {
+        console.warn("clipboard not supported")
+        link.classList.add("copied-error")
+        setTimeout(() => (link.classList.remove("copied-error")), 3000)
+      }
     })
   }
 }
 
 async function writeClipboardText(text) {
-  if (!navigator?.clipboard) {
-    console.warn("clipboard not supported")
-    return
-  }
+  if (!navigator?.clipboard) return false
   try {
     await navigator.clipboard.writeText(text);
+    return true
   } catch (error) {
     console.error(error.message);
+    return false
   }
 }

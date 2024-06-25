3b4d377f49a5a079d0e258b6c5c59d37c22df8c2
stordahl <jacob@stordahl.dev>
2024-06-25 01:17:18 +0000

    update content

--- src/pages/consulting.tsx
+++ src/pages/consulting.tsx
@@ -2,13 +2,13 @@ export function Consulting() {
 	return (
 		<>
       <h1>Third Party JavaScript Consulting</h1>
-      <p>Unlock the full potential of your web applications with expert guidance on Third Party JavaScript development. Whether you're integrating APIs, widgets, or analytics tools, I'd love to work with your team to ensure modern developer experience, seamless integration, and optimized performance.</p>
+      <p>Unlock the full potential of your web software with expert guidance on Third Party JavaScript development. Whether you are looking to build SDKs, widgets, or analytics tools, I'd love to work with your team to ensure modern developer experience, seamless integration, and optimized performance.</p>
      
       <h2>Why Third Party JavaScript Matters</h2>
-      <p>In today's interconnected web ecosystem, leveraging Third Party JavaScript is crucial for enhancing functionality without reinventing the wheel. From embedding social media plugins to integrating complex analytics solutions, third-party scripts streamline development while enriching user experiences.</p>
+      <p>In today's interconnected web ecosystem, providing a Third Party JavaScript integration for your software can bring a lot of value to your business. From embedded widgets to complex analytics solutions, third-party scripts enrich user experiences while provided a platform agnostic integration target.</p>
 
       <h2>My Background</h2>
-      <p>Throughout my career, I've been dealing with a lot of Third Party code. From managing Third Party Integrations for the <a href="https://hennepintech.edu/">largest technical college in Minnesota</a>, to building Third Party JavaScript applications that appear on e-commerce sites like <a href="https://stylitics.com/resources/newsroom/stylitics-maximizes-retailers-catalogs-with-automated-styling-and-outfitting-solutions-2-2/">Puma</a>, <a href="https://stylitics.com/resources/case-studies/use-case-how-stylitics-is-helping-dicks-sporting-goods-effortlessly-create-featured-shops-and-merchandising-bundles/">Dick's Sporting Goods</a>, and West Elm. The roles I've had over the past 5 years have provided me time to go deep on the problem space of Third Party JavaScript, and now I'm looking to help teams navigate this fruitful though ambiguous product market.</p>
+      <p>Throughout my career, I've been dealing with a lot of Third Party code. From managing Third Party Integrations for the <a href="https://hennepintech.edu/">largest technical college in Minnesota</a>, to building Third Party JavaScript applications that live on e-commerce sites like <a href="https://stylitics.com/resources/newsroom/stylitics-maximizes-retailers-catalogs-with-automated-styling-and-outfitting-solutions-2-2/">Puma</a> and <a href="https://stylitics.com/resources/case-studies/use-case-how-stylitics-is-helping-dicks-sporting-goods-effortlessly-create-featured-shops-and-merchandising-bundles/">Dick's Sporting Goods</a>. Currently, I'm a Product Engineer at <a href="https://stylitics.com">Stylitics</a>, which has provided me time to go deep on the problem space of Third Party JavaScript, and now I'm looking to help other teams navigate this fruitful though ambiguous product market.</p>
 
       <div class="card card-dash">
         <p>If you're intersted in the services I offer, shoot me an <a href="mailto:jacob@stordahl.dev">email</a>.</p>
@@ -21,15 +21,15 @@ export function Consulting() {
         <p style="font-style: italic;">I will provide guidance on application architecture, tooling, and workflows. We'll explore how developing a Third Party application can fit into your teams existing workflows, and design an architecture that gives maximum power to you team.</p>
       </div>
 
-      <div class="card">
+      {/*<div class="card">
         <h3>Work Shops</h3>
-        <p style="font-style: italic">I'll provide a day long work shop where I will build a Third Party JavaScript application with your team. We'll cover tooling, development workflow, deloyment, and application integration. The application will use modern web development tooling such as TypeScript and Vite, as well as your teams JavaScript framework of choice.</p>
-      </div>
+        <p style="font-style: italic">I will host a day long work shop where we will build a Third Party JavaScript application. We'll cover tooling, development workflow, and application integration. The application will use modern web development tooling such as TypeScript and Vite, as well as your teams JavaScript framework of choice.</p>
+      </div>*/}
 		</>
 	);
 }
 
 export const ConsultingMeta = {
-	title: "Hire Me!",
+	title: "Consulting",
 	description: "",
 };
